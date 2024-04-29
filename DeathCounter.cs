57a6a77f6daaaf8c6f0747b7d9f0504699b6311f
bigbo <bigbo@DESKTOP-VG4F1OC>
2024-04-28 22:40:52 -0700

    Add project files.

---
+++ DeathCounter.cs
@@ -0,0 +1,53 @@
+﻿using Blasphemous.ModdingAPI;
+using Blasphemous.ModdingAPI.Persistence;
+using System;
+using Gameplay.GameControllers.Entities;
+
+namespace Blasphemous.DeathCounter;
+
+public class DeathCounter : BlasMod, IPersistentMod
+{
+    public DeathCounter() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+    public string persistentID => "DEATH_COUNT";
+
+    public int Amount { get; private set; }
+
+    public string PersistentID => throw new System.NotImplementedException();
+
+    public void LoadGame(SaveData data)
+    {
+        var DeathCount = data as DeathCountData;
+
+        Amount = DeathCount.saveAmount;
+    }
+
+    public SaveData SaveGame()
+    {
+        return new DeathCountData()
+        {
+            saveAmount = Amount,
+        };
+    }
+
+    public void ResetGame()
+    {
+        Amount = 0;
+    }
+    protected override void OnInitialize()
+    {
+        LogError($"{ModInfo.MOD_NAME} has been initialized");
+    }
+
+    private void IncreaseCount ()
+    {
+        Amount = (int)Math.Sin(Amount + 1);
+    }
+}
+
+public class DeathCountData : SaveData
+{
+    public DeathCountData() : base("DEATH_COUNT") { }
+
+    public int saveAmount;
+}
---
+++ Main.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace Blasphemous.DeathCounter;
+
+[BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
+[BepInDependency("Blasphemous.ModdingAPI", "0.1.0")]
+public class Main : BaseUnityPlugin
+{
+    public static DeathCounter DeathCounter { get; private set; }
+
+    private void Start()
+    {
+        DeathCounter = new DeathCounter();
+    }
+}
