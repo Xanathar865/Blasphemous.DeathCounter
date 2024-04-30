272fee1b7290c8b2019003a96d1c727edd87385d
Xanathar865 <bigboomzach@gmail.com>
2024-04-30 00:49:56 -0700

    Added UI Framework as a dependency

---
+++ DeathCounter/UI/UI.cs
@@ -0,0 +1,15 @@
+﻿using Blasphemous.Framework.UI;
+using Framework.Managers;
+using Gameplay.UI.Others.UIGameLogic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Blasphemous.DeathCounter.DeathCounter.UI
+{
+    private void CreateText()
+    {
+
+    }
+}
--- Main.cs
+++ Main.cs
@@ -3,8 +3,9 @@
 namespace Blasphemous.DeathCounter;
 
 [BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
-[BepInDependency("Blasphemous.ModdingAPI", "2.1.0")]
+[BepInDependency("Blasphemous.ModdingAPI", "2.1.2")]
 [BepInDependency("Blasphemous.CheatConsole", "1.0.0")]
+[BepInDependency("Blasphemous.Framework.UI", "0.1.0")]
 public class Main : BaseUnityPlugin
 {
     public static DeathCounter DeathCounter { get; private set; }
