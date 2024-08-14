72616bfc8d795a9eb1178e4e2d00798c89e592f4
Xanathar865 <bigboomzach@gmail.com>
2024-08-14 11:19:16 -0700

    First patch attempt

--- DeathCountPatches.cs
+++ DeathCountPatches.cs
@@ -1,1 +1,17 @@
+using Framework.Managers;
+using Framework.Map;
+using Gameplay.UI.Others.MenuLogic;
+using HarmonyLib;
+using System.Collections.Generic;
 
+namespace Blasphemous.DeathCounter;
+
+//Creates OnMapUpdate Method
+[HarmonyPatch(typeof(NewMapMenuWidget), "OnMapUpdate")]
+class MapMenuWidetShow_Patch
+{
+    public static void PostFix()
+    {
+        Main.DeathCounter.OnMapUpdate();
+    }
+}
--- DeathCounter.cs
+++ DeathCounter.cs
@@ -71,7 +71,7 @@ public class DeathCount : BlasMod, IPersistentMod
     }
 
     //Display text if it isn't already displayed
-    protected override void OnUpdate()
+    public void OnMapUpdate()
     {
         if (DeathText == null)
             CreateText();
